4e900a8760e06b17fb133b61390e793bbfb3bf37
ian60509 <ian.60509@gmail.com>
2024-11-10 09:20:53 +0000

    feat: restore test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,23 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Define a function to print a separator that matches the terminal width
-# print_separator() {
-#     local message=" $1 " # Get the function argument as the message
-#     local term_width=$(tput cols) # Get the terminal width
-#     local equals_width=$(( (term_width - ${#message}) / 2 )) # Calculate the number of equal signs
+# Define a function to print a separator that matches the terminal width
+print_separator() {
+    local message=" $1 " # Get the function argument as the message
+    local term_width=$(tput cols) # Get the terminal width
+    local equals_width=$(( (term_width - ${#message}) / 2 )) # Calculate the number of equal signs
 
-#     # Output the separator line
-#     printf '%*s' "$equals_width" | tr ' ' '='
-#     printf '%s' "$message"
-#     printf '%*s\n' "$equals_width" | tr ' ' '='
-# }
+    # Output the separator line
+    printf '%*s' "$equals_width" | tr ' ' '='
+    printf '%s' "$message"
+    printf '%*s\n' "$equals_width" | tr ' ' '='
+}
 
-# print_separator "Check files change"
-# sh ./.github/scripts/check_files.sh
+print_separator "Check files change"
+sh ./.github/scripts/check_files.sh
 
-# print_separator "Install requirements"
-# cd "/mnt/app"
-# pip install --quiet --no-cache-dir -r requirements.txt
+print_separator "Install requirements"
+cd "/mnt/app"
+pip install --quiet --no-cache-dir -r requirements.txt
 
 print_separator "Run pytest"
 cd /mnt
@@ -26,11 +26,4 @@ mkdir test
 cp -fr .github/scripts/* ./test/
 cp -fr app/* ./test/
 cd /mnt/test
-#pytest
-
-
-
-
-pytest test_homework4.py
-
-rm -rf /mnt/test
+pytest
